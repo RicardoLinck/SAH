82c6f23880f3a037721fa03eeb7cf7d66981a243
Ricardo  Linck <ricardo.linck@eldorado.org.br>
2016-11-03 16:08:24 -0200

    Adding angular controller to index page.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
-function indexCtrl ($scope){
-    alert($);
-}
+ angular.module("sah", [])
+    .controller('indexCtrl',function($scope) {
+        $scope.submit = function(){
+
+            if(!validateLogin()){
+                $scope.emailValidationResult = "";
+                return false;
+            }
+
+            $scope.emailValidationResult = 'hidden';
+            location.href = "selecao-perfil.html";
+        }
+
+        $scope.emailValidationResult = 'hidden';
+
+        var validateLogin = function(){
+            return $scope.model.email == 'teste@teste.com' && $scope.model.password == '123';
+        }
+    });
